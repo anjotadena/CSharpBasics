64f7395ba62a103ea85ca8470f851b24ff586291
Anjo Tadena <tadena.anjo@gmail.com>
2022-09-03 21:15:27 +0800

    C# Basics

---
+++ Fundametals/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's youre name?");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello");
+            Console.WriteLine(userName);
+        }
+    }
+}
